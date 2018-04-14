1e7900a9e20d4cb8dcf4c35da101cac01867bfbf
harry8242 <mohamedharis97@gmail.com>
2018-04-14 15:38:39 +0530

    CMParts: Remove Keydisabler dependency for soft nav-bar
    *Just until keydisabler is fixed on the kernel side

--- src/org/cyanogenmod/cmparts/input/ButtonSettings.java
+++ src/org/cyanogenmod/cmparts/input/ButtonSettings.java
@@ -262,23 +262,19 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         // Only visible on devices that does not have a navigation bar already,
         // and don't even try unless the existing keys can be disabled
         boolean needsNavigationBar = false;
-        if (hardware.isSupported(CMHardwareManager.FEATURE_KEY_DISABLE)) {
-            try {
-                IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
-                needsNavigationBar = wm.needsNavigationBar();
-            } catch (RemoteException e) {
-            }
+        try {
+             IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+             needsNavigationBar = wm.needsNavigationBar();
+        } catch (RemoteException e) {
+        }
 
-            if (needsNavigationBar) {
-                prefScreen.removePreference(mDisableNavigationKeys);
-            } else {
-                // Remove keys that can be provided by the navbar
-                updateDisableNavkeysOption();
-                mNavigationPreferencesCat.setEnabled(mDisableNavigationKeys.isChecked());
-                updateDisableNavkeysCategories(mDisableNavigationKeys.isChecked());
-            }
+        if (needsNavigationBar) {
+             prefScreen.removePreference(mDisableNavigationKeys);
         } else {
-            prefScreen.removePreference(mDisableNavigationKeys);
+             // Remove keys that can be provided by the navbar
+             updateDisableNavkeysOption();
+             mNavigationPreferencesCat.setEnabled(mDisableNavigationKeys.isChecked());
+             updateDisableNavkeysCategories(mDisableNavigationKeys.isChecked());
         }
 
         if (hasPowerKey) {
